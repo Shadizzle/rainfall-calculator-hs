f0002612a22097948c8a6b9764e6ebc8f2cbecc8
Shadizzle <mail@shayden.me>
2018-01-29 18:12:10 +0930

    Added min and max, handle invalid input and added a burgeoning reporting system

--- rainfall.hs
+++ rainfall.hs
@@ -1,24 +1,58 @@
 {-# LANGUAGE ParallelListComp #-}
 
 import System.Environment (getArgs)
+import Text.Read (readMaybe)
+import Data.List (intercalate)
+import qualified Data.Map.Strict as Map
 
-select :: (a -> Bool) -> (a -> b) -> (a -> b) -> a -> b
-select p t f x = if p x then t x else f x
+average :: (Integral a, Fractional b) => [a] -> b
+average xs = sum (fmap fromIntegral xs) / fromIntegral (length xs)
 
-average :: (Fractional a) => [a] -> a
-average xs = sum xs / fromIntegral (length xs)
+type Rain = [Int]
+type ReportGenerator = Map.Map String (Rain -> ReportField)
+newtype Report = Report (Map.Map String ReportField)
+data ReportField = IntField Int
+                 | FloatField Float
+                 | StringField String
+                 | EmptyField
+
+instance Show Report where
+  show (Report report) = intercalate ", " [ name ++ ": " ++ show field
+                                          | (name, field) <- Map.toList report ]
+
+instance Show ReportField where
+  show (IntField i) = show i
+  show (FloatField f) = show f
+  show (StringField s) = s
+  show EmptyField = "N/A"
+
+normalize :: [Int] -> Rain
+normalize = filter (>= 0)
+          . takeWhile (/= (-999))
+
+(|?|) :: (a -> ReportField) -> (Rain -> a) -> Rain -> ReportField
+(|?|) fieldType transform rain = if null rain
+                               then EmptyField
+                               else (fieldType . transform) rain
+
+reportsWith :: ReportGenerator -> Rain -> Report
+reportsWith generator rain = Report (Map.map ($ rain) generator)
+
+report :: Rain -> Report
+report = reportsWith stdReport
+  where stdReport = Map.fromList
+                  [ ("Avg", FloatField |?| average)
+                  , ("Max", IntField   |?| maximum)
+                  , ("Min", IntField   |?| minimum)
+                  ]
 
 main = do
   args <- getArgs
-  rain <- return $ map (normalize . read) args
-  putStr $ concat [ list ++ " -> " ++ "Avg: " ++ avg ++ ", "
-                                   ++ "Max: " ++ max ++ ", "
-                                   ++ "Min: " ++ min ++ "\n"
+  putStr $ concat [ list ++ " -> " ++ report ++ "\n"
                   | list <- args
-                  | avg  <- map (maybe "N/A" (show . average)) rain
-                  | max  <- map (maybe "N/A" (show . maximum)) rain
-                  | min  <- map (maybe "N/A" (show . minimum)) rain]
-  where
-    normalize = select null (const Nothing) Just
-              . filter (>= 0)
-              . takeWhile (/= (-999))
+                  | report <- runReports args ]
+  where runReports = fmap ( maybe "Invalid Input" show
+                          . fmap report
+                          . fmap normalize
+                          . readMaybe
+                          )
